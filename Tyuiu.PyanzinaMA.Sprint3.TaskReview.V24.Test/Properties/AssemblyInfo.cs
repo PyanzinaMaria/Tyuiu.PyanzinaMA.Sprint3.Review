2ec0ecf76f99837d06ffadf3bf452fa6415f1e1d
Tyuiu.PyanzinaMA@outlook.com <mrpnzn@yaandex.ru>
2023-11-01 14:32:44 +0500

    Add project files.

---
+++ Tyuiu.PyanzinaMA.Sprint3.TaskReview.V24.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PyanzinaMA.Sprint3.TaskReview.V24.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PyanzinaMA.Sprint3.TaskReview.V24.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("30f83f55-7c7c-47f9-aeda-c1ff5f0af3af")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PyanzinaMA.Sprint3.TaskReview.V24/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.PyanzinaMA.Sprint3.TaskReview.V24.Lib;
+
+namespace Tyuiu.PyanzinaMA.Sprint3.TaskReview.V24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнила: Пьянзина М. А. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: TaskReview                                                        *");
+            Console.WriteLine("* Задание TaskReview                                                      *");
+            Console.WriteLine("* Вариант #24                                                             *");
+            Console.WriteLine("* Выполнила: Пьянзина Мария Алексеевна | ИИПб-23-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Напишите программу, которая выводит таблицу значений функции (произвести*");
+            Console.WriteLine("* табулирование) f(x) на заданном диапазоне [-5;5] с шагом 1. Произвести  *");
+            Console.WriteLine("* проверку деления на ноль. При делении на ноль вернуть значение 0.       *");
+            Console.WriteLine("* Значения занести в массив. Значения округлить до 2 знаков после запятой *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+            double[] valueArray;
+            valueArray = new double[len];
+
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("+--------+-----------+");
+            Console.WriteLine("|   X    +   f(x)    |");
+            Console.WriteLine("+--------+-----------+");
+
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("|{0,5:d}   |   {1, 6:f2}  |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+--------+-----------+");
+
+            Console.ReadKey();
+        }
+    }
+}
